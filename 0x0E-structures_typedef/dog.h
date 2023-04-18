22fe3a8c420b1a824997affcaeb1f816979d2ef4
ndivhu45 <ndivhu2456@gmail.com>
2023-04-18 16:46:03 -0700

    new structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * structure user
+ *@1: name
+ *@2: age
+ *@3: owner
+ *
+ *Return: 0.
+ */
+struct User
+{
+	char *name;
+	double age; 
+	char *owner;
+}
+int main(void)
+{
+struct User user;
+
+ 	struct dog my_dog;
+	my_dog.name ="poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = Bob;
+
+	return (0);
+
