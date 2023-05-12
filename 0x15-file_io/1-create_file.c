4d01fe78546551031572cad481f8ae8c43b44f55
ndivhu45 <ndivhu2456@gmail.com>
2023-05-12 06:00:33 -0700

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, letters, wr
+	int fd, letters, wr;
 
 	if (filename == NULL)
 		return (-1);
@@ -23,7 +23,6 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 	for (letters = 0; text_content[letters]; letters++)
-		;
 
 	wr = write(fd, text_content, letters);
 
