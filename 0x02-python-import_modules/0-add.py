0dc530cae2b3e4322bb7e477f8930ef69e9f6164
Ndivhu45 <ndivhu2456@gmail.com>
2023-08-11 05:46:09 -0700

    Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,2 +1,14 @@
-def add(a, b):
-    return a + b
+#!/usr/bin/python3
+
+from add_0 import add
+
+def main():
+    a = 1
+    b = 2
+
+    result = add(a, b)
+
+    print(f"{a} + {b} = {result}")
+
+if __name__ == "__main__":
+    main()
